6360ec5f460f372854e6b6409177399341042c5b
GummiBlip <gummiblip@gmail.com>
2023-05-02 17:29:08 -0500

    Draw grid

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 let container = document.querySelector("#container");
 
 for (i = 0; i < 16; i++) {
-  let div = document.createElement("div");
-  div.classList.add("gridBox");
-  container.appendChild(div);
+  let subContainer = document.createElement("div");
+  subContainer.classList.add("rowContainer");
+  container.appendChild(subContainer);
+  for (j = 0; j < 16; j++) {
+    let div = document.createElement("div");
+    div.classList.add("gridBox");
+    subContainer.appendChild(div);
+  }
+
 }
