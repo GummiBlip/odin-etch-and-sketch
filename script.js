a4304866c3607382ab404520eafa33e0a3bc6132
GummiBlip <gummiblip@gmail.com>
2023-05-04 12:13:10 -0500

    Implement grid scaling

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-let container = document.querySelector("#gridContainer");
-
-for (i = 0; i < 16; i++) {
-  let subContainer = document.createElement("div");
-  subContainer.classList.add("rowContainer");
-  container.appendChild(subContainer);
-  for (j = 0; j < 16; j++) {
-    let div = document.createElement("div");
-    div.classList.add("gridBox");
-    div.addEventListener("mouseover", () => {div.style.backgroundColor = "red";})
-    subContainer.appendChild(div);
-  }
-
+function getSize() {
+  let size = prompt("Please enter a grid size");
+  return size;
 }
+
+function generateGrid(gridSize, containerSize) {
+  for (i = 0; i < gridSize; i++) {
+    let subContainer = document.createElement("div");
+    subContainer.classList.add("rowContainer");
+    container.appendChild(subContainer);
+    for (j = 0; j < gridSize; j++) {
+      let div = document.createElement("div");
+      div.style.width = `${ (1/gridSize) * containerSize}px`;
+      div.style.height = `${ (1/gridSize) * containerSize}px`;
+      div.classList.add("gridBox");
+      div.addEventListener("mouseover", () => {div.style.backgroundColor = "red";})
+      subContainer.appendChild(div);
+    }
+  }
+}
+
+let container = document.querySelector("#gridContainer");
+let promptButton = document.querySelector("#sizeButton");
+let size = 64;
+
+promptButton.addEventListener("click", generateGrid(getSize, 960));
+
+generateGrid(size, 960);
+
